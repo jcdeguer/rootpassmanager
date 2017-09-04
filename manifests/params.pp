eec01f526e693f83f458579e804344ea7ae0ce26
Juan Carlos <juan.deguer@gmail.com>
2017-09-04 15:19:37 -0300

    Create params.pp

---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+# Class: rootpassmanager::params
+#
+# This class provides a few parameters.
+#
+
+class rootpassmanager::params {
+
+# Parametros
+
+  $change_enable        = false
+  $root_user_name       = "root"
+  $root_password        = "Nueva123"
+
+}
